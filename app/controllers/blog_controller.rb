498fc7e82c542d3b04f09c1760c7ff4005f072e0
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-03-30 11:02:54 -0700

    Added delete method

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -18,6 +18,12 @@ class BlogController < ApplicationController
         end
     end
 
+    def destroy
+        @blog = Blog.find(params[:id])
+        if @blog.destroy
+            redirect_to blogs_path
+        end
+    end
 
     private    
     def blog_params
