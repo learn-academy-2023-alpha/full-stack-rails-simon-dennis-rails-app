625161e049143647acea933fffd7c41615b79f34
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-03-30 11:32:02 -0700

    Added a edit page and functionallity

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/blogs/:id' => 'blog#shows' , as: "blog"
   post 'blogs' => 'blog#create'
   delete '/blogs/:id' => 'blog#destroy', as: "delete"
+  get '/blogs/:id/edit' => 'blog#edit', as: "edit"
 end
 
